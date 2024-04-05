e839acf05b3a7e015ea94550f94f1658cb33b456
FerminOsi <xPlash.business@gmail.com>
2024-04-05 02:34:40 +0200

    No hace falta importar el metodo ya que los disparos los cambiamos al crear y asignarle un arma

--- src/main/java/com/aetxabao/invasoresfx/sprite/EnemyAspersor.java
+++ src/main/java/com/aetxabao/invasoresfx/sprite/EnemyAspersor.java
@@ -12,15 +12,4 @@ public class EnemyAspersor extends EnemyShipDiagonal  {
     public EnemyAspersor(Rect gameRect, Image img, int N) {
         super(gameRect, img, N);
     }
-
-
-
-    public ArrayList<AShot> shoot() {
-        ArrayList<AShot> list = new ArrayList<>();
-        if (weapon!=null) {
-            list = weapon.shoot(this);
-        }
-        return list;
-
     }
-}
